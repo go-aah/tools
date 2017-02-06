48b733392a79ea78bbf45195be329b9999fd690d
Jeevanandam M <jeeva@myjeeva.com>
2017-02-06 09:15:18 -0800

    common util file for reusable method

---
+++ aah/util.go
@@ -0,0 +1,126 @@
+// Copyright (c) Jeevanandam M. (https://github.com/jeevatkm)
+// go-aah/tools source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"aahframework.org/aah"
+	"aahframework.org/config"
+	"aahframework.org/essentials"
+	"aahframework.org/log"
+)
+
+func importPathRelwd() string {
+	pwd, _ := os.Getwd()
+	importPath, _ := filepath.Rel(gosrcDir, pwd)
+	return filepath.ToSlash(importPath)
+}
+
+func getNonEmptyAbsPath(patha, pathb string) string {
+	configPath, err := filepath.Abs(firstNonEmpty(patha, pathb))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return configPath
+}
+
+func firstNonEmpty(a, b string) string {
+	r := a
+	if !ess.IsStrEmpty(b) {
+		r = b
+	}
+	return r
+}
+
+// getAppVersion method returns the aah application version, which used to display
+// version from compiled bnary
+// 		$ appname version
+//
+// Application version value priority are -
+// 		1. Env variable - AAH_APP_VERSION
+// 		2. git describe
+// 		3. version number from aah.project file
+func getAppVersion(appBaseDir string, cfg *config.Config) string {
+	// From env variable
+	if version := os.Getenv("AAH_APP_VERSION"); !ess.IsStrEmpty(version) {
+		return version
+	}
+
+	// fallback version number from file aah.project
+	version := cfg.StringDefault("version", "")
+
+	// git describe
+	if gitcmd, err := exec.LookPath("git"); err == nil {
+		appGitDir := filepath.Join(appBaseDir, ".git")
+		if !ess.IsFileExists(appGitDir) {
+			return version
+		}
+
+		gitArgs := []string{fmt.Sprintf("--git-dir=%s", appGitDir), "describe", "--always", "--dirty"}
+		output, err := execCmd(gitcmd, gitArgs)
+		if err != nil {
+			return version
+		}
+
+		version = strings.TrimSpace(output)
+	}
+
+	return version
+}
+
+// getBuildDate method returns application build date, which used to display
+// version from compiled bnary
+// 		$ appname version
+//
+// Application build date value priority are -
+// 		1. Env variable - AAH_APP_BUILD_DATE
+// 		2. Created with time.Now().Format(time.RFC3339)
+func getBuildDate() string {
+	// From env variable
+	if buildDate := os.Getenv("AAH_APP_BUILD_DATE"); !ess.IsStrEmpty(buildDate) {
+		return buildDate
+	}
+
+	return time.Now().Format(time.RFC3339)
+}
+
+func execCmd(cmdName string, args []string) (string, error) {
+	cmd := exec.Command(cmdName, args...)
+	log.Info("Executing ", strings.Join(cmd.Args, " "))
+
+	bytes, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("\n%s\n%s", string(bytes), err)
+	}
+
+	return string(bytes), nil
+}
+
+func renderTmpl(w io.Writer, text string, data interface{}) {
+	tmpl := template.Must(template.New("").Parse(text))
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Fatalf("Unable to render template text: %s", err)
+	}
+}
+
+// createAppBinaryName method binary name creation
+func createAppBinaryName(buildCfg *config.Config) string {
+	name := strings.Replace(buildCfg.StringDefault("name", aah.AppName()), " ", "_", -1)
+	appBinaryName := buildCfg.StringDefault("build.binary_name", name)
+	if isWindows {
+		appBinaryName += ".exe"
+	}
+
+	return filepath.Join(gopath, "bin", "aah.d", aah.AppImportPath(), appBinaryName)
+}
